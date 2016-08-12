2df2f9fe17288f77e1ffa9fb4ca29e9bc0b4496c
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-08-12 15:07:55 +0600

    peach: Add OpenDelta OTA App & cleanup

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd ../../../..
-cd packages/apps/CMUpdater
-git apply -v ../../../device/ark/peach/patches/updater.patch
-cd ../../..
 cd device/cyanogen/msm8916-common/
 git apply -v ../../../device/ark/peach/patches/nodtb.patch
 cd ../../..

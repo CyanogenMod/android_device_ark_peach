24e4dd7125cba5f93a3c32217d003df5f5d6ef36
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-08-15 21:59:34 +0600

    peach: patches: rename & fix script

--- patches/apply.sh
+++ patches/apply.sh
@@ -3,4 +3,5 @@ cd ../../../..
 cd device/cyanogen/msm8916-common/
 git apply -v ../../../device/ark/peach/patches/nodtb.patch
 git apply -v ../../../device/ark/peach/patches/sensors.patch
+cd ../../..
 echo "Patches applying succesfully"

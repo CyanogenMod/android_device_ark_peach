a104c9c0499e96a56e3ca42fbe64b6274b0fada7
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-08-15 20:30:01 +0600

    peach: may be fix sensors

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,5 +2,5 @@
 cd ../../../..
 cd device/cyanogen/msm8916-common/
 git apply -v ../../../device/ark/peach/patches/nodtb.patch
-cd ../../..
+git apply -v ../../../device/ark/peach/patches/sensors.patch
 echo "Patches applying succesfully"

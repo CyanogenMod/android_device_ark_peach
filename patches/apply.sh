9ef7abcbd11456be5c58b9af7649398e0b030c80
lolmaxlik <ilya.lebedev.2003@mail.ru>
2016-08-16 12:36:01 +0600

    peach: patches: add freq fix patch

--- patches/apply.sh
+++ patches/apply.sh
@@ -3,5 +3,6 @@ cd ../../../..
 cd device/cyanogen/msm8916-common/
 git apply -v ../../../device/ark/peach/patches/nodtb.patch
 git apply -v ../../../device/ark/peach/patches/sensors.patch
+git apply -v ../../../device/ark/peach/patches/freq.patch
 cd ../../..
 echo "Patches applying succesfully"

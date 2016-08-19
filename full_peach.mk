27a80199eed3f788dd1b03903feb3479c0b13f5b
Ilya <ilya.lebedev.2003@mail.ru>
2016-08-19 12:56:58 +0600

    Update full_peach.mk

--- full_peach.mk
+++ full_peach.mk
@@ -18,7 +18,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from seed device
-$(call inherit-product, device/akr/peach/device.mk)
+$(call inherit-product, device/ark/peach/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := peach
